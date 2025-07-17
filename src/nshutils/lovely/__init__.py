180c54fdafa2c8bc96e8a582903e94332b3024c6
Nima Shoghi <nimashoghi@gmail.com>
2025-07-17 13:40:17 -0700

    feat(lovely): Add pretty-printing utilities
    Adds `pformat` and `pprint` from the `lovely` submodule, which allows formatting without global monkey patching.

--- src/nshutils/lovely/__init__.py
+++ src/nshutils/lovely/__init__.py
@@ -1,12 +1,15 @@
 from __future__ import annotations
 
+from ._monkey_patch_all import monkey_patch as lovely_monkey_patch
 from ._monkey_patch_all import monkey_patch as monkey_patch
 from .config import LovelyConfig as LovelyConfig
 from .jax_ import jax_monkey_patch as jax_monkey_patch
 from .jax_ import jax_repr as jax_repr
 from .numpy_ import numpy_monkey_patch as numpy_monkey_patch
 from .numpy_ import numpy_repr as numpy_repr
+from .pprint_ import pformat as pformat
+from .pprint_ import pprint as pprint
 from .torch_ import torch_monkey_patch as torch_monkey_patch
 from .torch_ import torch_repr as torch_repr
 
-lovely_monkey_patch = monkey_patch
+_ = lovely_monkey_patch
